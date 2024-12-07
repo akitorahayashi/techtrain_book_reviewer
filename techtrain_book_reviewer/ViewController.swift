61f8aa35a731c192d408cd76d3cf72fd06b62eba
akitora <hayashi20030703@gmail.com>
2024-12-07 13:08:51 +0900

    Initial commit

---
+++ techtrain_book_reviewer/ViewController.swift
@@ -0,0 +1,17 @@
+//
+//  ViewController.swift
+//  techtrain_book_reviewer
+//
+//  Created by 林 明虎 on 2024/12/07.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+}
+
---
+++ techtrain_book_reviewerUITests/techtrain_book_reviewerUITestsLaunchTests.swift
@@ -0,0 +1,33 @@
+//
+//  techtrain_book_reviewerUITestsLaunchTests.swift
+//  techtrain_book_reviewerUITests
+//
+//  Created by 林 明虎 on 2024/12/07.
+//
+
+import XCTest
+
+final class techtrain_book_reviewerUITestsLaunchTests: XCTestCase {
+
+    override class var runsForEachTargetApplicationUIConfiguration: Bool {
+        true
+    }
+
+    override func setUpWithError() throws {
+        continueAfterFailure = false
+    }
+
+    @MainActor
+    func testLaunch() throws {
+        let app = XCUIApplication()
+        app.launch()
+
+        // Insert steps here to perform after app launch but before taking a screenshot,
+        // such as logging into a test account or navigating somewhere in the app
+
+        let attachment = XCTAttachment(screenshot: app.screenshot())
+        attachment.name = "Launch Screen"
+        attachment.lifetime = .keepAlways
+        add(attachment)
+    }
+}
