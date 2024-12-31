d0b4dfa91329175cebe61da4223abb0adcd902b8
akitora <hayashi20030703@gmail.com>
2024-12-31 10:01:46 +0900

    UserProfileServiceをswift concurrncyに対応させた

--- techtrain_book_reviewer/Service/UserProfileService.swift
+++ techtrain_book_reviewer/Service/UserProfileService.swift
@@ -7,7 +7,7 @@
 
 import Foundation
 
-class UserProfileService {
+actor UserProfileService {
     
     static var yourAccount: TBRUser? = nil
     
@@ -17,13 +17,27 @@ class UserProfileService {
         self.apiClient = apiClient
     }
     
+    private func decodeUserProfile(token: String, profileData: Data) throws(TechTrainAPIError) -> TBRUser {
+        do {
+            if let jsonUserData = try JSONSerialization.jsonObject(with: profileData, options: []) as? [String: Any],
+               let name = jsonUserData["name"] as? String,
+               let iconUrl = jsonUserData["iconUrl"] as? String {
+                let user = TBRUser(token: token, name: name, iconUrl: iconUrl)
+                return user
+            } else {
+                throw TechTrainAPIError.decodingError
+            }
+        } catch {
+            throw TechTrainAPIError.decodingError
+        }
+    }
+    
     
     /// ユーザー名を更新する
     func updateUserName(
         withToken token: String,
-        newName: String,
-        completion: @escaping (Result<Void, TechTrainAPIError.ServiceError>) -> Void
-    ) {
+        newName: String
+    ) async throws(TechTrainAPIError.ServiceError) -> Void {
         let endpoint = "/users"
         
         let headers = [
@@ -34,14 +48,11 @@ class UserProfileService {
             "name": newName
         ]
         
-        apiClient.makeRequest(to: endpoint, method: "PUT", body: parameters, headers: headers) { result in
-            switch result {
-            case .success:
-                print("UserProfileService: ユーザー名の更新に成功しました")
-                completion(.success(()))
-            case .failure(let error):
-                completion(.failure(error.toServiceError()))
-            }
+        do {
+            let _ = try await apiClient.makeRequestAsync(to: endpoint, method: "PUT", headers: headers, body: parameters)
+            print("UserProfileService: ユーザー名の更新に成功しました")
+        } catch {
+            throw error.toServiceError()
         }
     }
     
@@ -49,35 +60,19 @@ class UserProfileService {
     func fetchUserProfile(
         withToken token: String,
         completion: @escaping (Result<Void, TechTrainAPIError.ServiceError>) -> Void
-    ) {
+    ) async throws(TechTrainAPIError.ServiceError) -> Void {
         let endpoint = "/users"
         
         let headers = [
             "Authorization": "Bearer \(token)"
         ]
         
-        apiClient.makeRequest(to: endpoint, method: "GET", body: nil, headers: headers) { result in
-            switch result {
-            case .success(let data):
-                do {
-                    print("レスポンスデータ: \(String(data: data, encoding: .utf8) ?? "データなし")")
-                    if let json = try JSONSerialization.jsonObject(with: data, options: []) as? [String: Any],
-                       let name = json["name"] as? String,
-                       let iconUrl = json["iconUrl"] as? String? {
-                        let user = TBRUser(token: token, name: name, iconUrl: iconUrl)
-                        UserProfileService.yourAccount = user
-                        completion(.success(()))
-                    } else {
-                        print("JSON解析失敗")
-                        completion(.failure(.underlyingError(.decodingError)))
-                    }
-                } catch {
-                    print("JSON解析失敗")
-                    completion(.failure(.underlyingError(.decodingError)))
-                }
-            case .failure(let error):
-                completion(.failure(error.toServiceError()))
-            }
+        do {
+            let profileData = try await apiClient.makeRequestAsync(to: endpoint, method: "GET", headers: headers, body: nil)
+            let decodedUserData = try decodeUserProfile(token: token, profileData: profileData)
+            UserProfileService.yourAccount = decodedUserData
+        } catch {
+            throw error.toServiceError()
         }
     }
 }
